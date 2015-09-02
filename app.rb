c9c2d3b0f1f529780e9c605291acad4b18dd4897
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-02 18:15:56 +0100

    Handle POST request from form and printing back on screen

--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'sinatra/reloader' if development?
 require_relative './models/begins_with_vowel'
 require_relative './models/begins_with_consonant'
 
-get '/:word' do
-  "#{Capitalize.cap params[:word]}"
+get '/' do
+  @title = 'My Pig Latin Translator'
+  erb :home
+end
+
+post '/' do
+  # binding.pry // Stops Sinatra process
+  # puts params // Puts hash keys and values which come with POST request
+  
+  word = params[:translator_input].downcase
+  
+  @translation = Capitalize.cap word
+  
+  # puts @translation // this, puts word in terminal
+
+  erb :home # Calls home.erb page that gets new variables like @translation
 end
--- models/begins_with_consonant.rb
+++ models/begins_with_consonant.rb
@@ -1,4 +1,12 @@
-class ConsonantStarting
+#class ConsonantStarting
+
+#  def self.cap word
+#    word[0].upcase.concat word[1..word.length]
+#  end#
+
+#end
+
+class Capitalize
 
   def self.cap word
     word[0].upcase.concat word[1..word.length]
